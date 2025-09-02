10fbb22b006e191af25c6ff2b37a6011494d59e3
Abhishek Agrahari <abhishek9670177034@gmail.com>
2025-09-02 10:59:28 +0530

    Update TechStack.jsx
    change hover effect

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -89,7 +89,6 @@ const TechStack = () => {
 
         .tech-card:hover {
           transform: scale(1.05);
-          box-shadow: 0 0 10px rgba(0, 204, 255, 0.15);
         }
 
         .tech-card:hover .tech-icon {
